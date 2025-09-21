129a436de2710757f09c077648ef647b88e2276c
_Mark <markopsec.dev.main@gmail.com>
2025-09-21 14:10:53 +0800

    ewqeweqw

--- godot-proj-main/Scripts/main scene script/deviceCheck.gd
+++ godot-proj-main/Scripts/main scene script/deviceCheck.gd
@@ -1,17 +1,21 @@
 extends Node2D
 
-func is_mobile() -> bool:
-	var platform := OS.get_name()
-	return platform == "Android" or platform == "iOS"
+func is_mobile_browser() -> bool:
+	# Check if it's running as Web first
+	if OS.get_name() != "Web":
+		return false
 
-func is_desktop() -> bool:
-	var platform := OS.get_name()
-	return platform == "Windows" or platform == "Linux" or platform == "macOS"
+	# Heuristic: mobile browsers usually report a smaller screen width
+	var screen_size = DisplayServer.screen_get_size()
+	return screen_size.x <= 768  # 768px is a common breakpoint for tablets/phones
+
+func is_desktop_browser() -> bool:
+	return OS.get_name() == "Web" and not is_mobile_browser()
 
 func _ready():
-	if is_mobile():
-		print("Running on Mobile")
-	elif is_desktop():
-		print("Running on Desktop")
+	if is_mobile_browser():
+		print("Running on Mobile Browser")
+	elif is_desktop_browser():
+		print("Running on Desktop Browser")
 	else:
 		print("Other platform: ", OS.get_name())
